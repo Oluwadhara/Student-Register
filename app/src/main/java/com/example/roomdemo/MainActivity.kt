c80ab5662becb243f6e5444cfd4476aa876f6542
Oluwadhara <olufemioluwadara@gmail.com>
2024-04-10 12:33:30 +0100

    initial commit

---
+++ app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.example.roomdemo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.roomdemo.databinding.ActivityMainBinding
+import com.example.roomdemo.db.Subscriber
+import com.example.roomdemo.db.SubscriberDatabase
+import com.example.roomdemo.db.SubscriberRepository
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var subscriberViewModel: SubscriberViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        val dao = SubscriberDatabase.getInstance(application).SubscriberDAO
+        val repository = SubscriberRepository(dao)
+        val factory = SubscriberViewModelFactory(repository)
+        subscriberViewModel = ViewModelProvider(this,factory)[SubscriberViewModel::class.java]
+        binding.myViewModel = subscriberViewModel
+        binding.lifecycleOwner = this
+        initRecyclerView()
+
+        subscriberViewModel.message.observe(this, Observer {
+            it.getContentIfNotHandled()?.let {
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+
+    private fun initRecyclerView(){
+        binding.subscriberRecyclerView.layoutManager = LinearLayoutManager(this)
+        displaySubscribersList()
+    }
+
+    private fun displaySubscribersList(){
+        subscriberViewModel.subscribers.observe(this, Observer {
+            Log.i("MY-TAG",it.toString())
+            binding.subscriberRecyclerView.adapter = MyRecyclerViewAdapter(it) { selectedItem: Subscriber ->
+                listItemClicked(
+                    selectedItem
+                )
+            }
+        })
+    }
+
+    private fun listItemClicked(subscriber: Subscriber){
+        // Toast.makeText(this, "Selected name is: ${subscriber.name}",Toast.LENGTH_LONG).show()
+        subscriberViewModel.initUpdateAndDelete(subscriber)
+    }
+}
